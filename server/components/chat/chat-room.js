ae3b4545f5f9463f2b4e4d1a2a3fb1831a2aab4c
Christian <ffchris1@gmail.com>
2015-11-17 23:16:01 -0430

    chat changes

--- server/components/chat/chat-room.js
+++ server/components/chat/chat-room.js
@@ -190,7 +190,7 @@ module.exports = function (io) {
           io.sockets.in(room).emit('user disconnected',{username:user.username,nickName:user.nickName});
         }
 
-      }, 30000);
+      }, 10000);
 
       //socket.broadcast.to(room).emit('user down',data.user);
     });
